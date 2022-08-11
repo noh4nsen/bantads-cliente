69dfc0407524eccf7cd98563c4e816a71b32f46d
Raphael Rodrigues <raphael.rodrigues@wisereducacao.com>
2022-08-11 20:21:59 -0300

    first commit

---
+++ src/main/java/com/bantads/cliente/bantadscliente/BantadsClienteApplication.java
@@ -0,0 +1,13 @@
+package com.bantads.cliente.bantadscliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BantadsClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BantadsClienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bantads/cliente/bantadscliente/BantadsClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bantads.cliente.bantadscliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BantadsClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
